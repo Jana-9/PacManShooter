05b1761c6ee3be105266dd543a0be1ec8b805fcc
Jana-9 <133791510+Jana-9@users.noreply.github.com>
2024-04-20 15:51:35 +0300

    Update Player.java

--- src/Game/Player.java
+++ src/Game/Player.java
@@ -3,207 +3,164 @@ package Game;
 import static Game.Game.mouseY;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
-import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Graphics;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.awt.Point;
 import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 
 /**
- * Rappresenta il giocatore sia in single player che in multiplayer.
+ * Represents the player in the game, both in single player and multiplayer modes.
  * @author carloblasi
  */
-public class Player {
+public interface Player {
+    public void render(Graphics g);
+    public void loadImage();
+    public void update(Input input, int delta);
+    public void detectCollisionWithEnemies(ArrayList<Enemy> enemies) throws SlickException;
+    public void detectCollisionWithBullet(ArrayList<Bullet> bullets) throws SlickException;
+    public void checkIfPickedUpAmmos(Ammo[] ammos) throws SlickException;
 
+    public void reset();
+
+    public void removeAmmo();
+
+    public int getAmmos();
+
+    public boolean isAlive();
+
+    public int getX();
+
+    public int getY();
+
+    public int getHealth();
+
+    public Point getCoordinates();
+}
+
+class PacMan implements Player {
     private Image playerImage;
     private int health = 100;
     private int rotation = 0, radius = 22;
-    private Point Coordinates = new Point(Window.WIDTH/2, Window.HEIGHT/2);
+    private Point coordinates = new Point(Window.WIDTH / 2, Window.HEIGHT / 2);
     private final int speed = 324, damage = 10;
     private boolean alive = true;
-    private HealthBar healthbar;
+    private HealthBar healthBar;
     private int ammos = 50;
     private int ID = Game.players++;
 
-    /**
-     * Inizializza il giocatore.
-     */
-    public Player() {
-
+    public PacMan() {
         loadImage();
-        this.healthbar = new HealthBar(0, 0);
+        this.healthBar = new HealthBar(0, 0);
     }
 
-    /**
-     * Disegna il giocatore se è vivo.
-     * @param g L'oggetto grafico che permette di disegnare su schermo
-     */
+    @Override
     public void render(Graphics g) {
-
         if (isAlive()) {
-
-            this.playerImage.drawCentered(this.Coordinates.x, this.Coordinates.y);
-            this.playerImage.setRotation((float)this.rotation);
-            this.healthbar.render(this.Coordinates.x - 24, this.Coordinates.y - 50,/*(Window.WIDTH * this.getHealth()) / 100*/this.getHealth()/2, g);
+            this.playerImage.drawCentered(this.coordinates.x, this.coordinates.y);
+            this.playerImage.setRotation((float) this.rotation);
+            this.healthBar.render(this.coordinates.x - 24, this.coordinates.y - 50, this.health / 2, g);
         }
     }
 
-    /**
-     * Carica l'immagine.
-     */
+    @Override
     public void loadImage() {
-
         try {
             this.playerImage = new Image("Images/Pac Man.png");
         } catch (SlickException e) {
-            System.out.println("playerImage not found");
+            e.printStackTrace();
         }
     }
 
-    /**
-     * Aggiorna la posizione del giocatore in base all'input fornito.
-     * @param input L'oggetto che si occupa degli input da tastiera e da mouse
-     * @param delta {@code delta} del gioco
-     */
+    @Override
     public void update(Input input, int delta) {
-
         if (this.alive) {
+            this.rotation = (int) getAngleFromPoint(new Point(input.getMouseX(), input.getMouseY()), this.coordinates);
+            int speed = (this.speed * delta / 1000);
 
-            this.rotation = (int)getAngleFromPoint(new Point(input.getMouseX(), input.getMouseY()), this.Coordinates);
-            int speed = (this.speed * delta/1000);
+            if (input.isKeyDown(Input.KEY_D))
+                this.coordinates.x += speed;
+            if (input.isKeyDown(Input.KEY_A))
+                this.coordinates.x -= speed;
+            if (input.isKeyDown(Input.KEY_W))
+                this.coordinates.y -= speed;
+            if (input.isKeyDown(Input.KEY_S))
+                this.coordinates.y += speed;
 
-            // RIGHT
-            if(input.isKeyDown(Input.KEY_D))
-                this.Coordinates.x += speed;
-
-            // LEFT
-            if(input.isKeyDown(Input.KEY_A))
-                this.Coordinates.x -= speed;
-
-            // UP
-            if(input.isKeyDown(Input.KEY_W))
-                this.Coordinates.y -= speed;
-
-            // DOWN
-            if(input.isKeyDown(Input.KEY_S))
-                this.Coordinates.y += speed;
-
-            if(this.Coordinates.y >= Window.HEIGHT - 45)
-                this.Coordinates.y = Window.HEIGHT - 45;
-            if(this.Coordinates.y <= 45)
-                this.Coordinates.y = 45;
-            if(this.Coordinates.x >= Window.WIDTH - 45)
-                this.Coordinates.x = Window.WIDTH - 45;
-            if(this.Coordinates.x <= 45)
-                this.Coordinates.x = 45;
+            if (this.coordinates.y >= Window.HEIGHT - 45)
+                this.coordinates.y = Window.HEIGHT - 45;
+            if (this.coordinates.y <= 45)
+                this.coordinates.y = 45;
+            if (this.coordinates.x >= Window.WIDTH - 45)
+                this.coordinates.x = Window.WIDTH - 45;
+            if (this.coordinates.x <= 45)
+                this.coordinates.x = 45;
 
             if (health == 0)
                 this.alive = false;
         }
     }
 
-    /**
-     * Controlla se il giocatore ha colliso con un nemico e diminuisce la sua vita nel caso sia successo.
-     * Rimuove anche il nemico dalla lista che lo contiene.
-     * @param enemies La lista che contiene i nemici ancoara vivi
-     * @throws SlickException
-     */
+    @Override
     public void detectCollisionWithEnemies(ArrayList<Enemy> enemies) throws SlickException {
-
         try {
             Iterator<Enemy> iter = enemies.iterator();
-
             while (iter.hasNext()) {
                 Enemy enemy = iter.next();
-
-                if (this.health != 0)
-                    if(enemy.isAlive())
-                        if (Math.sqrt((enemy.getX() - this.getX()) * (enemy.getX() - this.getX()) +
-                                      (enemy.getY() - this.getY()) * (enemy.getY() - this.getY())) <=
-                                       this.radius + enemy.getRadius()) {
-
-                            iter.remove();
-                            this.hit();
-                        }
+                if (this.health != 0 && enemy.isAlive() &&
+                        Math.sqrt(Math.pow((enemy.getX() - this.coordinates.getX()), 2) +
+                                Math.pow((enemy.getY() - this.coordinates.getY()), 2)) <= this.radius + enemy.getRadius()) {
+                    iter.remove();
+                    this.hit();
+                }
             }
         } catch (ConcurrentModificationException e) {
-            System.out.println("Poteva andare peggio...");
+            System.out.println("Error: Concurrent Modification");
         }
     }
 
-    /**
-     * Controlla se il giocatore ha colliso con un proiettile e diminuisce la sua vita nel caso sia successo.
-     * @param bullets La lista che contiene i proiettili sparati
-     * @throws SlickException
-     */
+    @Override
     public void detectCollisionWithBullet(ArrayList<Bullet> bullets) throws SlickException {
-
         Iterator<Bullet> iter = bullets.iterator();
-
         while (iter.hasNext()) {
             Bullet bullet = iter.next();
-
-            if (this.isAlive())
-                if (Math.sqrt((bullet.getX() - this.getX()) * (bullet.getX() - this.getX()) +
-                              (bullet.getY() - this.getY()) * (bullet.getY() - this.getY())) <=
-                               this.radius + bullet.getRadius()) {
-
-                    this.hit();
-                    iter.remove();
-                }
+            if (this.isAlive() &&
+                    Math.sqrt(Math.pow((bullet.getX() - this.coordinates.getX()), 2) +
+                            Math.pow((bullet.getY() - this.coordinates.getY()), 2)) <= this.radius + bullet.getRadius()) {
+                this.hit();
+                iter.remove();
+            }
         }
     }
 
-    /**
-     * Controlla se ha preso delle munizioni e aumenta le munizioni a disposizione se è successo.
-     * @param ammos Il vettore che contiene le munizioni da prendere
-     * @throws SlickException
-     */
+    @Override
     public void checkIfPickedUpAmmos(Ammo[] ammos) throws SlickException {
-
         for (Ammo ammo : ammos) {
-            if(this.health != 0)
-                if (!ammo.alreadyPicked())
-                    if (Math.sqrt((ammo.getX() - this.getX()) * (ammo.getX() - this.getX()) +
-                                  (ammo.getY() - this.getY()) * (ammo.getY() - this.getY())) <=
-                                   this.radius + ammo.getRadius()) {
-                        ammo.pick();
-                        new Sound("Sounds/pacmanPickedAmmo.wav").play();
-                        this.ammos += 50;
-                    }
+            if (this.health != 0 && !ammo.alreadyPicked() &&
+                    Math.sqrt(Math.pow((ammo.getX() - this.coordinates.getX()), 2) +
+                            Math.pow((ammo.getY() - this.coordinates.getY()), 2)) <= this.radius + ammo.getRadius()) {
+                ammo.pick();
+                new Sound("Sounds/pacmanPickedAmmo.wav").play();
+                this.ammos += 50;
+            }
         }
     }
 
-    /**
-     * Calcola l'angolo (compreso tra 0 e 359 inclusi) formato dalla posizione del giocatore e dalla posizione del puntatore.
-     * @param firstPoint Coordinate del giocatore
-     * @param secondPoint Coordinate del puntatore
-     * @return l'angolo
-     */
     private double getAngleFromPoint(Point firstPoint, Point secondPoint) {
-
         double r;
-
-        if (((secondPoint.x) > firstPoint.x)) //above 0 to 180 degrees
+        if (secondPoint.x > firstPoint.x)
             r = (Math.atan2((secondPoint.x - firstPoint.x), (firstPoint.y - secondPoint.y)) * 180 / Math.PI) + 90;
-
-        else if (((secondPoint.x) < firstPoint.x)) //above 180 degrees to 360/0
+        else if (secondPoint.x < firstPoint.x)
             r = 360 - (Math.atan2((firstPoint.x - secondPoint.x), (firstPoint.y - secondPoint.y)) * 180 / Math.PI) + 90;
-
         else
-            r = Math.atan2(0 ,0) + 90;
-
-        if (r == 90 && mouseY < this.Coordinates.y)
+            r = Math.atan2(0, 0) + 90;
+        if (r == 90 && mouseY < this.coordinates.y)
             return 270;
-
         return r;
     }
 
-    /**
-     * Colpisce il giocatore e gli toglie un po' di vita.
-     */
     public void hit() {
         this.health -= this.damage;
     }
@@ -213,11 +170,11 @@ public class Player {
     }
 
     public int getX() {
-        return (int)this.Coordinates.getX();
+        return (int) this.coordinates.getX();
     }
 
     public int getY() {
-        return (int)this.Coordinates.getY();
+        return (int) this.coordinates.getY();
     }
 
     public boolean isAlive() {
@@ -225,7 +182,7 @@ public class Player {
     }
 
     public Point getCoordinates() {
-        return this.Coordinates;
+        return this.coordinates;
     }
 
     public int getAmmos() {
@@ -240,15 +197,95 @@ public class Player {
         return this.ID;
     }
 
-    /**
-     * Ripristina le condizioni iniziali del giocatore.
-     */
     public void reset() {
-
         this.alive = true;
         this.health = 100;
         this.ammos = 50;
         loadImage();
-        this.Coordinates = new Point(Window.WIDTH/2, Window.HEIGHT/2);
+        this.coordinates = new Point(Window.WIDTH / 2, Window.HEIGHT / 2);
+    }
+}
+
+
+
+/**
+ * Adapter class to adapt PacMan to Player interface.
+ */
+
+ class PacManAdapter implements Player {
+  private MsPacMan msPacMan;
+
+    public PacManAdapter(MsPacMan newMsPacMan) {
+        msPacMan = newMsPacMan;
+    }
+
+    @Override
+    public void render(Graphics g) {
+        msPacMan.renderr(g);
+    }
+
+    @Override
+    public void loadImage() {
+        msPacMan.loadImagee();
+    }
+
+    @Override
+    public void update(Input input, int delta) {
+        msPacMan.updatee(input, delta);
+    }
+
+    @Override
+    public void detectCollisionWithEnemies(ArrayList<Enemy> enemies) throws SlickException {
+        msPacMan.detectCollisionWithEnemiess(enemies);
+    }
+
+    @Override
+    public void detectCollisionWithBullet(ArrayList<Bullet> bullets) throws SlickException {
+        msPacMan.detectCollisionWithBullett(bullets);
+    }
+
+    @Override
+    public void checkIfPickedUpAmmos(Ammo[] ammos) throws SlickException {
+        msPacMan.checkIfPickedUpAmmoss(ammos);
+    }
+
+    @Override
+    public void reset() {
+        msPacMan.resett();
+    }
+
+    @Override
+    public void removeAmmo() {
+        msPacMan.removeAmmoo();
+    }
+
+    @Override
+    public int getAmmos() {
+        return msPacMan.getAmmoss();
+    }
+
+    @Override
+    public boolean isAlive() {
+        return msPacMan.isAlivee();
+    }
+
+    @Override
+    public int getX() {
+        return msPacMan.ggetX();
+    }
+
+    @Override
+    public int getY() {
+        return msPacMan.ggetY();
+    }
+
+    @Override
+    public int getHealth() {
+        return msPacMan.ggetHealth();
+    }
+
+    @Override
+    public Point getCoordinates() {
+        return msPacMan.getCoordinatess();
     }
 }
