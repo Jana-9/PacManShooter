0724e97285512653ed4526f8d887eaef7c2c00f0
Jana-9 <133791510+Jana-9@users.noreply.github.com>
2024-05-04 22:47:54 +0300

    Update and rename Enemy.java to EnemyFactory.java

--- src/Game/EnemyFactory.java
+++ src/Game/EnemyFactory.java
@@ -7,6 +7,12 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Iterator;
+import static Game.GamePlayState.startPlayX;
+import static Game.GamePlayState.startPlayY;
+import static Game.MultiplayerGamePlayState.MultiplayerstartX;
+import static Game.MultiplayerGamePlayState.MultiplayerstartY;
+import static Game.MultiplayerGamePlayState.imageIndex;
+import static Game.MultiplayerGamePlayState.playerToFollow;
 
 /**
  * Interface for representing an enemy in the game.
@@ -54,23 +60,22 @@ interface Enemy {
     // Sets the speed of the enemy
     void setSpeed(int newSpeed);
 
-    // Gets the ID of the player to follow
-    int getPlayerToFollow();
-}
+}  
 
 /**
  * Public class representing a factory to create instances of Enemy.
  */
 public class EnemyFactory {
-    // Creates a default enemy with specified start coordinates
-    public static Enemy createEnemy(int startX, int startY) {
-        return new GhostEnemy(startX, startY);
-    }
 
-    // Creates an enemy with specified start coordinates, image index, and player to follow
-    public static Enemy createEnemy(int startX, int startY, int imageIndex, int playerToFollow) {
-        return new GhostEnemy(startX, startY, imageIndex, playerToFollow);
-    }
+    public Enemy getEnemy (String type) {
+        if(type.equalsIgnoreCase("multiplayerEnemy")){
+        return new MultiplayerGhostEnemy();
+        }
+        else if(type.equalsIgnoreCase("PlayEnemy")){
+             return new GhostEnemy();
+        }
+    return null;
+}
 }
 
 /**
@@ -85,16 +90,154 @@ class GhostEnemy implements Enemy {
     private float dy;
     private int diameter = 84;
     private int radius = diameter / 2;
-    private int playerToFollow;
+  
 
-    public GhostEnemy(int startX, int startY) {
-        this.coordinates.setLocation(startX, startY);
+    public GhostEnemy() {
+        this.coordinates.setLocation(startPlayX, startPlayY);
         loadImage();
+        System.out.print(startPlayY);
     }
 
-    public GhostEnemy(int startX, int startY, int imageIndex, int playerToFollow) {
-        this.coordinates.setLocation(startX, startY);
-        this.playerToFollow = playerToFollow;
+    
+
+    @Override
+    public void update(Point playerCoordinates, int delta) {
+        float rad = (float) (Math.atan2(playerCoordinates.x - this.coordinates.getX(), this.coordinates.getY() - playerCoordinates.y));
+        this.dx = (float) Math.sin(rad) * this.speed;
+        this.dy = -(float) Math.cos(rad) * this.speed;
+
+        float x = (float) this.coordinates.getX();
+        float y = (float) this.coordinates.getY();
+        x += this.dx * delta / 1000;
+        y += this.dy * delta / 1000;
+
+        this.alive = true;
+        this.coordinates.setLocation(x, y);
+    }
+
+    @Override
+    public void render() {
+        if (this.isAlive())
+            this.ghostImage.drawCentered((float) this.coordinates.getX(), (float) this.coordinates.getY());
+    }
+
+    @Override
+    public void detectCollisionWithBullet(ArrayList<Bullet> bullets) {
+        Iterator<Bullet> iter = bullets.iterator();
+        while (iter.hasNext()) {
+            Bullet bullet = iter.next();
+            if (this.isAlive() && bullet.isFired())
+                if (Math.sqrt((bullet.getX() - this.getX()) * (bullet.getX() - this.getX()) +
+                        (bullet.getY() - this.getY()) * (bullet.getY() - this.getY())) <=
+                        this.radius + bullet.getRadius()) {
+                    this.alive = false;
+                    Score.incrementScore();
+                    iter.remove();
+                }
+        }
+    }
+
+    @Override
+    public boolean isCollidingWithBullets(ArrayList<Bullet> bullets) {
+        Iterator<Bullet> iter = bullets.iterator();
+        while (iter.hasNext()) {
+            Bullet bullet = iter.next();
+            if (this.isAlive() && bullet.isFired())
+                if (Math.sqrt((bullet.getX() - this.getX()) * (bullet.getX() - this.getX()) +
+                        (bullet.getY() - this.getY()) * (bullet.getY() - this.getY())) <=
+                        this.radius + bullet.getRadius()) {
+                    Score.incrementScore();
+                    iter.remove();
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isCollidingWithBullets(LinkedList<Bullet> bullets) {
+        Iterator<Bullet> iter = bullets.iterator();
+        while (iter.hasNext()) {
+            Bullet bullet = iter.next();
+            if (this.isAlive() && bullet.isFired())
+                if (Math.sqrt((bullet.getX() - this.getX()) * (bullet.getX() - this.getX()) +
+                        (bullet.getY() - this.getY()) * (bullet.getY() - this.getY())) <=
+                        this.radius + bullet.getRadius()) {
+                    Score.incrementScore();
+                    iter.remove();
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    @Override
+    public void loadImage() {
+        Random r = new Random();
+        this.ghostImage = Game.ghosts1[r.nextInt(4)];
+    }
+
+    @Override
+    public void loadImage(int imageIndex) {
+        this.ghostImage = Game.ghosts1[imageIndex];
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.alive;
+    }
+
+    @Override
+    public int getX() {
+        return (int) this.coordinates.getX();
+    }
+
+    @Override
+    public int getY() {
+        return (int) this.coordinates.getY();
+    }
+
+    @Override
+    public int getRadius() {
+        return this.radius;
+    }
+
+    @Override
+    public void kill() {
+        this.alive = false;
+        Score.incrementScore();
+    }
+
+    @Override
+    public void remove() {
+        this.alive = false;
+    }
+
+    @Override
+    public void setSpeed(int newSpeed) {
+        this.speed = newSpeed;
+    }
+
+   
+}
+
+class  MultiplayerGhostEnemy implements Enemy {
+    private Image ghostImage;
+    private Point coordinates = new Point(0, 0);
+    private boolean alive = false;
+    private float speed = 168;
+    private float dx;
+    private float dy;
+    private int diameter = 84;
+    private int radius = diameter / 2;
+ 
+
+   
+
+    public MultiplayerGhostEnemy() {
+      
+        this.coordinates.setLocation(MultiplayerstartX, MultiplayerstartY);
+      
         loadImage(imageIndex);
     }
 
@@ -172,12 +315,12 @@ class GhostEnemy implements Enemy {
     @Override
     public void loadImage() {
         Random r = new Random();
-        this.ghostImage = Game.ghosts[r.nextInt(4)];
+        this.ghostImage = Game.ghosts2[r.nextInt(4)];
     }
 
     @Override
     public void loadImage(int imageIndex) {
-        this.ghostImage = Game.ghosts[imageIndex];
+        this.ghostImage = Game.ghosts2[imageIndex];
     }
 
     @Override
@@ -216,8 +359,8 @@ class GhostEnemy implements Enemy {
         this.speed = newSpeed;
     }
 
-    @Override
+ 
     public int getPlayerToFollow() {
-        return this.playerToFollow;
+        return playerToFollow;
     }
 }
