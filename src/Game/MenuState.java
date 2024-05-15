ef61bfaf67e9a8d8ac6f991b28bfd6b84de09909
Jana-9 <133791510+Jana-9@users.noreply.github.com>
2024-05-15 16:15:47 +0300

    Update MenuState.java

--- src/Game/MenuState.java
+++ src/Game/MenuState.java
@@ -13,75 +13,89 @@ import org.newdawn.slick.Input;
  * Rappresenta la scena del menu del gioco.
  * @author carloblasi
  */
-public class MenuState {
+ interface InputHandlerStrategy {
+    void handleInput(GameContainer gc, Input input, int mouseX, int mouseY);
+}
 
-    /**
-     * Metodo generico per aggiornare la logica degli oggetti della scena
-     * @param gc {@code GameContainer} del gioco
-     * @param input L'input del gioco
-     * @param delta {@code delta} del gioco
-     * @param mouseX Coordinata X del mouse
-     * @param mouseY Coordinata Y del mouse
-     */
-    public static void update(GameContainer gc, Input input, int delta, int mouseX, int mouseY) {
-
+ class MouseClickHandler implements InputHandlerStrategy {
+     
+    @Override
+    public void handleInput(GameContainer gc, Input input, int mouseX, int mouseY) {
+        
         if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
-
+            
             if (playButton.isPressed()) {
-
                 Window.clear(input);
                 Game.openingSound.play();
                 Game.state = Game.MENUPACMAN;
-            }
-            if (multiplayerButton.isPressed()) {
-
+                
+            } else if (multiplayerButton.isPressed()) {
                 Window.clear(input);
                 Game.state = Game.MULTIPLAYERMENUSTATE;
                 Game.IPTextField.setText("");
                 Game.DestinationPortTextField.setText("");
                 Game.SourcePortTextField.setText("");
-            }
-            if (leaderboardButton.isPressed()) {
-
+                
+            } else if (leaderboardButton.isPressed()) {
                 Window.clear(input);
                 Game.state = Game.LEADERBOARDSTATE;
-            }
-            if (quitButton.isPressed()) {
+                
+            } else if (quitButton.isPressed()) {
                 System.exit(0);
+                
             }
         }
+    }
+}
 
+ class KeyPressHandler implements InputHandlerStrategy {
+     
+    @Override
+    public void handleInput(GameContainer gc, Input input, int mouseX, int mouseY) {
+        
         if (input.isKeyPressed(Input.KEY_ENTER)) {
-
             Window.clear(input);
             Game.openingSound.play();
             Game.state = Game.MENUPACMAN;
-        }
-        if (input.isKeyPressed(Input.KEY_M)) {
-
+            
+        } else if (input.isKeyPressed(Input.KEY_M)) {
             Window.clear(input);
             Game.state = Game.MULTIPLAYERMENUSTATE;
             Game.IPTextField.setText("");
             Game.DestinationPortTextField.setText("");
             Game.SourcePortTextField.setText("");
-        }
-        if (input.isKeyPressed(Input.KEY_L)) {
-
+            
+        } else if (input.isKeyPressed(Input.KEY_L)) {
             Window.clear(input);
             Game.state = Game.LEADERBOARDSTATE;
-        }
-        if (input.isKeyPressed(Input.KEY_ESCAPE)) {
+            
+        } else if (input.isKeyPressed(Input.KEY_ESCAPE)) {
             System.exit(0);
+            
         }
+    }
+}
 
+public class MenuState {
+    
+    private InputHandlerStrategy InputHandler;
+
+    public MenuState(InputHandlerStrategy InputHandler) {
+        this.InputHandler = InputHandler;
+    }
+    
+
+    public void update(GameContainer gc, Input input, int delta, int mouseX, int mouseY) {
+        
+        InputHandler.handleInput(gc, input, mouseX, mouseY);
+        
         playButton.hoverEffect();
         multiplayerButton.hoverEffect();
         leaderboardButton.hoverEffect();
         quitButton.hoverEffect();
     }
 
-    public static void render(GameContainer gc, Graphics g) {
-
+    public void render(GameContainer gc, Graphics g) {
         titleImage.drawCentered(Window.HALF_WIDTH, Window.HEIGHT/5);
         playButton.render();
         multiplayerButton.render();
